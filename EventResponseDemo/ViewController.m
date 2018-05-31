d5a380c7c7a302fd60723847159a84fedbbb6c0e
luo6luo <767126162@qq.com>
2018-05-31 10:07:02 +0800

    - 修改错别字 - 上传实例图片

--- EventResponseDemo/ViewController.m
+++ EventResponseDemo/ViewController.m
@@ -35,7 +35,7 @@
      - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event;
      - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event;
      - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(nullable UIEvent *)event;
-     - (void)touchesEstimatedPropertiesUpdated:(NSSet<UITouch *> *)touches NS_AVAILABLE_IOS(9_1);
+     - (void)touchesEstimatedPropertiesUpdated:(NSSet<UITouch *> *)touches;
      2、加速计事件
      - (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event;
      - (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event;
@@ -45,10 +45,10 @@
      
      
      UITouch
-     触摸事件中，touches中存放的是UITouch对应；其中，一根手指对应一个UITouch对象
+     触摸事件中，touches中存放的是UITouch对象；其中，一根手指对应一个UITouch对象
      触摸开始，系统会创建手指对应的UITouch对象，随着手指移动，UITouch对象会随着更新，触摸事件结束，系统会销毁这个UITouch对象
      <一> 属性
-     1、window    --> 触摸产生时对应所处的窗口
+     1、window    --> 触摸产生时对应触摸的窗口
      2、view      --> 触摸产生时对应的触摸视图
      3、tapCount  --> 短时间的触摸次数，据此判断是几根手指触摸，双击、单击或者其他更多
      4、timestamp --> 触摸事件产生或者变化的时间（单位秒）
@@ -58,7 +58,7 @@
      7、majorRadius           --> 触摸的主半径
      8、majorRadiusTolerance  --> 触摸主半径的公差
      == iOS9中新增属性 ==
-     9、UITouchType           --> 触摸类型（直接手指触摸：Direct、间接触摸：Indirect、触摸笔触摸：Stylus）
+     9、type                  --> 触摸类型（直接手指触摸：Direct、间接触摸：Indirect、触摸笔触摸：Stylus）
      10、force                --> 触摸力度，其中1.0表示平均力压值
      11、maximumPossibleForce --> 能触摸的最大力度
      12、altitudeAngle        --> 高角度，仅适用于用触摸笔触摸类型。0：触摸笔平行于平面；M_PI/2：触摸笔垂直于平面
@@ -68,7 +68,7 @@
      <二> 方法
         // 返回触摸在view上的坐标位置（以view左上角原点(0, 0)为准），若传入的view = nil，则返回触摸点在window上的位置
      1、- (CGPoint)locationInView:(nullable UIView *)view;
-        //同上，这个方法记录了上一个触摸点的位置
+        // 同上，这个方法记录了上一个触摸点的位置
      2、- (CGPoint)previousLocationInView:(nullable UIView *)view;
      == iOS9中新增的方法 ==
         // 精准的返回触摸在view上的坐标位置
@@ -83,7 +83,7 @@
      
      UIEvent
      每产生一个事件就会产生一个UIEvent对象
-     UIEvent对象记录事件的产生时刻和类型
+     UIEvent对象记录了事件的产生时刻和类型
      <一>属性
      1、type --> 事件类型（Touches：点击、Motion：加速计、RemoteControl：远程控制、Presses：按压）
      2、subtype --> 同一事件类型的具体事件
@@ -101,7 +101,7 @@
           RemoteControlBeginSeekingForward：开始向前寻找（开始快进）
           RemoteControlEndSeekingForward：结束向前寻找（结束快进）
      3、timestamp --> 事件产生的时间
-     4、allTouches --> 事件中的产生的所有触摸点
+     4、allTouches --> 事件中产生的所有触摸点
      <二>
         // 返回在窗口上的所有触摸点
      1、- (nullable NSSet <UITouch *> *)touchesForWindow:(UIWindow *)window;
@@ -125,7 +125,7 @@
      
      触摸事件：事件的产生和传递
      1、事件发生后，系统会将事件加入到UIApplication管理的队列中
-     2、UIApplication遵循先进先出的原则，将队列中先进的时间发送给主窗口（keyWindow）
+     2、UIApplication遵循先进先出的原则，将队列中先进的事件发送给主窗口（keyWindow）
      3、主窗口会在视图层级中找到一个合适的view来接收和处理事件
      4、找到合适的view后，会调用视图的touches方法来具体处理事件
      
@@ -133,12 +133,12 @@
      寻找合适控件接收触摸事件步骤：
      1、先判断view是否能接收触摸事件
      2、触摸点是否在view身上
-     3、从后向前遍历子控件，然后重复上面两步，找到则返回合适的子控件
+     3、在子控件数组中从后向前遍历子控件，然后重复上面两步，找到则返回合适的子控件
      4、没找到合适的子控件，则自己就是合适处理的
      
      
      点击方法的内部实现方法：
-        // 时间一传递给控件，就会调用下列方法，返回最合适接收时间的UIView
+        // 事件一传递给控件，就会调用下列方法，返回最合适接收事件的UIView
      1、- (nullable UIView *)hitTest:(CGPoint)point withEvent:(nullable UIEvent *)event;
         // 默认值是 YES，返回触摸点是否在调用者身上
      2、- (BOOL)pointInside:(CGPoint)point withEvent:(nullable UIEvent *)event;
@@ -152,7 +152,7 @@
      4、最佳视图接收调用touches方法
      5、如果最佳视图调用了[super touches...]，则事件由下往上顺着响应者链传递上去
      6、接着就会调用上一个响应者的touches方法
-     7、如果上一个响应者不能影响，则传递给上上一个响应者
+     7、如果上一个响应者不能响应，则传递给上上一个响应者
      8、如果传递到UIApplication还是不能处理，那这个时间就会被废弃
      
      
